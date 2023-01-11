66fe148ae33e53f6e357933d0b757f13a19e0a31
jinji <jinjinlei2108@hotmail.com>
2023-01-11 13:07:24 +1100

    update server.js

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ function sleep(ms) {
 
 async function main() {
   while (true) {
+    const favoriteFood = process.env.FAVORITE_FOOD;
     console.log("My favorite food is ${favoriteFood}");
-    await sleep(5000);
+    return
   }
 }
-const favoriteFood = process.env.FAVORITE_FOOD;
+
 main();
