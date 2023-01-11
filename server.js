43fae037ccce95386f26d49c11dc3a7b263daa4e
jinji <jinjinlei2108@hotmail.com>
2023-01-11 15:19:42 +1100

    change the server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-
+const favoriteFood = process.env.FAVORITE_FOOD;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while (true) {
-    const favoriteFood = process.env.FAVORITE_FOOD;
-    console.log("My favorite food is ${favoriteFood}");
+    console.log(`My favorite food is ${favoriteFood}`);
     return
   }
 }
